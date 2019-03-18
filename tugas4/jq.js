aa2c99333ff9aa5225610fd9a769cfedced5a7bf
forderation <kharisma.muzaki@gmail.com>
2019-03-18 10:34:50 +0700

    ubah warna

--- tugas4/jq.js
+++ tugas4/jq.js
@@ -7,4 +7,7 @@ $(document).ready(function(){
     var nilai = $("#kotak").width();
     alert(nilai);
   });
+  $("#tombol3").click(function(){
+    $("#kotak").css("background-color","blue");
+  });
 });
